cf9c79e2be3f8bcfe9f39b2aba768d7ef3a65108
juan <juancamilogaviriaacosta@gmail.com>
2017-08-22 16:51:59 -0500

    Cypress 90%

--- cypress/cypress/integration/simple_spec.js
+++ cypress/cypress/integration/simple_spec.js
@@ -1,4 +1,5 @@
-describe('Los estudiantes login', function() {
+/*
+describe('Los estudiantes login exitoso', function() {
     it('Visits los estudiantes and fails at login', function() {
       cy.visit('https://losestudiantes.co')
       cy.contains('Cerrar').click()
@@ -10,3 +11,30 @@ describe('Los estudiantes login', function() {
     
     })
 })
+
+describe('Creacion de cuenta con usuario existente', function() {
+    it('Visits los estudiantes and fails at login', function() {
+      cy.visit('https://losestudiantes.co')
+      cy.contains('Cerrar').click()
+      cy.contains('Ingresar').click()
+      cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("pruebas")
+      cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("automaticas")
+      cy.get('.cajaSignUp').find('input[name="correo"]').click().type("cosmefulanitog88@gmail.com")
+      cy.get('.cajaSignUp').find('input[name="password"]').click().type("cosmefulanitog88")
+      cy.get('.cajaSignUp').contains('Registrarse').click()
+      cy.contains('Ocurrió un error activando tu cuenta')
+    
+    })
+})
+*/
+
+describe('Los estudiantes login exitoso', function() {
+    it('Visits los estudiantes and fails at login', function() {
+      cy.visit('https://losestudiantes.co')
+      cy.contains('Cerrar').click()
+      cy.get('.Select-placeholder').click()
+      cy.get('.Select-input').find('input[role="combobox"]').type("juan").wait('1000')
+      cy.get('.Select-menu-outer').click()
+    })
+})
+
