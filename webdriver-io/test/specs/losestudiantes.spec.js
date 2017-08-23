34e7529a7ee001415b82223a6ce63af8f4d65b20
juan <juancamilogaviriaacosta@gmail.com>
2017-08-23 01:01:57 -0500

    web driver prueba 1

--- webdriver-io/test/specs/losestudiantes.spec.js
+++ webdriver-io/test/specs/losestudiantes.spec.js
@@ -23,3 +23,28 @@ describe('los estudiantes login', function() {
         browser.click('#cuenta')
     });
 });
+
+describe('Creacion de cuenta con usuario existente', function() {
+    it('should visit los estudiantes and failed at log in', function () {
+        browser.url('https://losestudiantes.co');
+        browser.click('button=Cerrar');
+        browser.waitForVisible('button=Ingresar', 5000);
+        browser.click('button=Ingresar');
+
+        var cajaLogIn = browser.element('.cajaLogIn');
+        var mailInput = cajaLogIn.element('input[name="correo"]');
+
+        mailInput.click();
+        mailInput.keys('cosmefulanitog88@gmail.com');
+
+        var passwordInput = cajaLogIn.element('input[name="password"]');
+
+        passwordInput.click();
+        passwordInput.keys('cosmefulanitog88');
+
+        cajaLogIn.element('button=Ingresar').click()
+
+        browser.waitForVisible('#cuenta', 5000);
+        browser.click('#cuenta')
+    });
+});
